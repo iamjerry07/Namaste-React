d13720589652f90558577c168ffa6d46a4e06f76
Rohan <rohansingh8382@gmail.com>
2025-06-27 19:35:04 +0530

    completed

---
+++ App.js
@@ -0,0 +1,20 @@
+// let heading =  React.createElement("h1", {id: "firstID"}, "Hello world from react")
+
+ 
+
+    const parent = React.createElement("div", {id: "parent"},[
+         React.createElement("div", {id:"child"}, 
+            [React.createElement("h1", {},"I am a child") , 
+             React.createElement("h2", {},"I am a child 2")]
+            ),
+            React.createElement("div", {id:"child"}, 
+            [React.createElement("h1", {},"I am a child") , 
+             React.createElement("h2", {},"I am a child 2")]
+            )])
+
+
+
+    console.log(parent)
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+   root.render(parent)
+    
